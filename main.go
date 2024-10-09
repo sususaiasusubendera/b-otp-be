e315ce06d1dcf6e9d7975141ad986789674b7c6f
sususaiasusubendera <oplessnewbee@gmail.com>
2024-10-09 22:06:58 +0700

    docs: update comments main.go

--- main.go
+++ main.go
@@ -7,18 +7,20 @@ import (
 	"os"
 )
 
-// Struct
+// STRUCT
 type Page struct {
 	Title string
 	Body  []byte
 }
 
-// Function & Method
+// FUNCTION & METHOD
+// save method
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
+// loadPage function
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename)
@@ -28,13 +30,14 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// viewHandler function
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
     p, _ := loadPage(title)
     fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
-// Main
+// MAIN
 func main() {
     http.HandleFunc("/view/", viewHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))
