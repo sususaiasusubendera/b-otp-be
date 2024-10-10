5b1be2930d7442b046d41fd32fdd38345aa87a70
sususaiasusubendera <oplessnewbee@gmail.com>
2024-10-10 10:26:11 +0700

    learn: edit viewHandler for handling non-existing pages

--- main.go
+++ main.go
@@ -38,9 +38,13 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 // HANDLER
 // viewHandler function
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func viewHandler(w http.ResponseWriter, r *http.Request) lea{
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}	
 	renderTemplate(w, "view", p)
 }
 
